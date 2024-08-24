faeb653b9e4637877617398ad9cfd178f278af04
Sagar-Deve <12201352@students.koi.edu.au>
2024-08-24 22:05:13 +1000

    Create AdditionCalculator.cs

---
+++ AdditionCalculator.cs
@@ -0,0 +1,26 @@
+﻿// Exercise 3 
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Addition Calculator");
+        //Ask the user to enter the first number
+        Console.Write("Enter the first number:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        //Ask the user to enter the second number
+        Console.Write("Enter the second number:");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int result = a + b;
+
+        Console.WriteLine("The total is: " + result);
+
+        Console.Read();
+
+
+    }
+
+}
